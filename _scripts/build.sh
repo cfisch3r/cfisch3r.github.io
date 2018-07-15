c37d6b1c029fa240d52612b068fa1cdcc9e6a828
Christian Fischer <cf@fisch3r.de>
2018-07-15 22:33:23 +0200

    disable html proofer

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle exec jekyll build
-htmlproofer --assume-extension --url-ignore https://linkedin.com/in/christian-fischer-a89952102,http://deanattali.com/beautiful-jekyll/,https://isqi.org/de/,https://www.activemind.de/datenschutz/datenschutzhinweis-generator/ ./_site
+# htmlproofer --assume-extension --url-ignore https://linkedin.com/in/christian-fischer-a89952102,http://deanattali.com/beautiful-jekyll/,https://isqi.org/de/,https://www.activemind.de/datenschutz/datenschutzhinweis-generator/ ./_site
