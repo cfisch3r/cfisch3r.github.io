64a8f0b20645330306cea08c30d495711bbfe283
Christian Fischer <cf@fisch3r.de>
2018-12-13 13:49:13 +0100

    switch to sftp for deployment

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'github-pages'
+gem 'net-sftp'
 group :jekyll_plugins do
   gem "jekyll-paginate-v2"
 end
---
+++ _scripts/deploy.rb
@@ -0,0 +1,33 @@
+require 'net/sftp'
+
+class SftpDeployer
+
+	def initialize(baseDir,siteFolder,host,user,password)
+		@sftp = Net::SFTP.start(host,user,:password => password)
+		@baseDir = baseDir
+		@siteFolder = siteFolder
+	end
+
+	def upload(dir)
+		@sftp.session.exec!("rm -rf #{backup_site_path}")	
+		@sftp.rename!(site_path, backup_site_path) if (dir_entry_names().include?(@siteFolder) )
+		@sftp.mkdir!(@baseDir + "/" + @siteFolder, :permissions => 0755)
+		@sftp.upload!(dir,site_path)
+	end
+
+	def site_path
+		@baseDir + "/" + @siteFolder
+	end
+
+	def backup_site_path
+		site_path + ".bak"
+	end
+
+	def dir_entry_names
+		@sftp.dir.glob(@baseDir,"*",0).map {|n| n.name}
+	end
+
+end
+
+deployer = SftpDeployer.new("www/htdocs/w00d8526/agiledojo","newsite",ENV['SFTP_HOST'],ENV['SFTP_USER'],ENV['SFTP_PASSWORD'])
+deployer.upload("_site")
